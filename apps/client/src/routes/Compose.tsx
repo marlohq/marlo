98e9f913b27c2cafd7e775292957942782a9b48e
matthewp <361671+matthewp@users.noreply.github.com>
2025-10-24 12:36:38 +0000

    format [skip ci]

--- apps/client/src/routes/Compose.tsx
+++ apps/client/src/routes/Compose.tsx
@@ -238,7 +238,9 @@ function ComposeForm({
 						form={form}
 						editor={editor}
 						draftMessageId={message.id}
-						onSubmit={(data) => sendMutation.mutate({ ...data, body: finalizeTipTapHtml(data.body || '') })}
+						onSubmit={(data) =>
+							sendMutation.mutate({ ...data, body: finalizeTipTapHtml(data.body || '') })
+						}
 						lastSaved={lastSaved ? new Date(lastSaved) : undefined}
 						pending={sendMutation.isPending}
 						saving={saving}
